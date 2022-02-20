51b68e7d1832cfb07f8cae3ebf229a5c0cdfab8e
Anders Hansen-Haug <andershh_93@hotmail.com>
2022-02-20 12:10:01 +0100

    small change

--- Db/connection.js
+++ Db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url ='mongodb+srv://Anders:asd123@myfirstcluster.pmnkh.mongodb.net/jwt?retryWrites=true&w=majority';
+const url ='mongodb+srv://######:######@myfirstcluster.pmnkh.mongodb.net/jwt?retryWrites=true&w=majority';
 
 async function dbConnect() {
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
